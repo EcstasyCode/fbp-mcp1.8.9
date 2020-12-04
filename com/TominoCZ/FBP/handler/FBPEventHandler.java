65919664658b8bb5cf235d8a9ef5b7e1d024ddfa
EcstasyCode <67347965+EcstasyCode@users.noreply.github.com>
2020-12-04 21:44:16 +0100

    initial commit

---
+++ com/TominoCZ/FBP/handler/FBPEventHandler.java
@@ -0,0 +1,27 @@
+package com.TominoCZ.FBP.handler;
+
+import com.TominoCZ.FBP.FBP;
+import com.TominoCZ.FBP.particle.FBPParticleDigging;
+import com.TominoCZ.FBP.particle.FBPParticleManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.EffectRenderer;
+import net.minecraft.client.particle.IParticleFactory;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+
+public class FBPEventHandler {
+    Minecraft mc = Minecraft.getInstance();
+
+    public void onWorldLoadEvent() {
+
+    }
+
+    public void onEntityJoinWorldEvent(Entity entity, World world) {
+        if (entity == this.mc.thePlayer) {
+            FBP.fancyEffectRenderer = new FBPParticleManager(world, this.mc.getRenderManager().renderEngine, (IParticleFactory) new FBPParticleDigging.Factory());
+            if (FBP.originalEffectRenderer == null || (FBP.originalEffectRenderer != this.mc.effectRenderer && FBP.originalEffectRenderer != FBP.fancyEffectRenderer))
+                FBP.originalEffectRenderer = this.mc.effectRenderer;
+            this.mc.effectRenderer = (EffectRenderer) FBP.fancyEffectRenderer;
+        }
+    }
+}
